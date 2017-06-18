f6c505abcbcb72364e2b0ff48800472295737a97
Grant China <grant.china@spirent.com>
2017-06-17 21:57:26 -1000

    Bumped version to 0.3.0.

--- funclog/about.py
+++ funclog/about.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 __title__ = 'funclog'
 __summary__ = 'A Python function logger'
 __uri__ = 'https://github.com/gchina/funclog'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Grant China'
 __email__ = 'tikiboy@fastmail.fm'
 __license__ = 'MIT'
